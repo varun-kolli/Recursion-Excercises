985391048e551c0573021a7f800b817f10165a55
varun-kolli <vkolli@calpoly.edu>
2020-09-21 16:27:50 -0700

    commit attempt

--- perm_lex.py
+++ perm_lex.py
@@ -1,4 +1,22 @@
 # string -> List of strings
 # Returns list of permutations for input string
 # e.g. 'ab' -> ['ab', 'ba']; 'a' -> ['a']; '' -> []
-def perm_gen_lex(str_in):
+
+
+
+def perm_gen_lex(str_in, smt = "", l = []):
+    if smt == "" and len(l) > 0:
+        l.clear()
+    if str_in == "" and smt == "":
+        return l
+    if len(str_in) == 0:
+        l.append(smt)
+    for char in range(len(str_in)):
+        passer = smt + str_in[char]
+        simp_string = str_in.replace(str_in[char], "")
+        perm_gen_lex(simp_string, passer, l)
+    return l
+
+
+
+
--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -3,10 +3,21 @@ import perm_lex
 
 # Starter test cases - write more!
 
+
 class TestAssign1(unittest.TestCase):
 
     def test_perm_gen_lex(self):
-        self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
+        self.assertEqual(perm_lex.perm_gen_lex('ab'), ['ab', 'ba'])
+
+    def test_perm_gen_lex_01(self):
+        self.assertEqual(perm_lex.perm_gen_lex('abc'), ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+
+    def test_perm_gen_lex_02(self):
+        self.assertEqual(perm_lex.perm_gen_lex(''), [])
+
+    def test_perm_gen_lex_03(self):
+        self.assertEqual(perm_lex.perm_gen_lex('a'), ['a'])
+
 
 if __name__ == "__main__":
-        unittest.main()
+    unittest.main()
